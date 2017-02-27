c938a8819ea7e6d7d89f12a3a0818c2aade8b967
David Wilbanks <dwilbank@gmail.com>
2017-02-27 08:21:59 -0800

    add possibly redundant cors code

--- server/server.js
+++ server/server.js
@@ -17,6 +17,13 @@ var app = express();
 const cors = require('cors');
 app.use(cors());
 
+app.use(function(req,res,next){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    next();
+});
+
 app.use(bodyParser.json());
 // app.use(morgan('combined'));
 if (process.env.NODE_ENV !== 'production') {
